77bb3b88c473d234fc797d972f41802ffc789513
nessoaf <64707980+nessoaf@users.noreply.github.com>
2020-07-01 17:59:57 -0700

    installing some jsons

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/', (req,res) => {
+//     var dndUrl = `https://www.dnd5eapi.co/api/`
+//     var mtgUrl = `https://api.magicthegathering.io/v1/cards`
+//     axios.get(mtg).then(apiResponse => {
+//         var dnd = apiResponse.data.results;
+//         res.render('')
+//     })
+// })
